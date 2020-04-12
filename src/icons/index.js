cbbcd113720b0b1420474304fa2719d67929ad55
emresandikci <me@emresandikci.com>
2020-04-12 04:29:24 +0300

    icons added

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+export { default as IconArrow } from './IconArrow';
+export { default as IconCheckCircle } from './IconCheckCircle';
+export { default as IconClose } from './IconClose';
+export { default as IconError } from './IconError';
+export { default as IconSearch } from './IconSearch';
+export { default as IconWarning } from './IconWarning';
