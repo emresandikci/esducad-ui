8f152f77bd8427c439491a086c0dfaf69d2e7486
emresandikci <me@emresandikci.com>
2020-04-12 04:10:45 +0300

    carousel component added/completed

---
+++ src/components/carousel/carousel.story.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import Box from '../box';
+import Carousel from './index';
+import Heading from '../heading';
+import Text from '../text';
+
+export default {
+  title: 'Carousel',
+  parameters: {
+    componentSubtitle:
+      'When need a slide image,text or other stuff, Carousel will slide given items inside in it',
+  },
+  component: Carousel,
+};
+const images = [
+  'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+  'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg',
+  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+];
+console.log(Carousel);
+export const Exampe = () => {
+  const onStepChanged = step => console.log(step);
+  const renderCarousel = () => (
+    <Carousel width={[1]} onChange={onStepChanged}>
+      <Carousel.Item>
+        <img src={images[0]} width="100%" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={images[1]} width="100%" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={images[2]} width="100%" />
+      </Carousel.Item>
+    </Carousel>
+  );
+  return (
+    <Box height="auto" bg="lightGray" py={6}>
+      {renderCarousel()}
+    </Box>
+  );
+};
+
+export const WithDuration = () => {
+  const onStepChanged = step => console.log(step);
+  const renderCarousel = () => (
+    <Carousel width={[1]} delay={3} onChange={onStepChanged}>
+      <Carousel.Item>
+        <img src={images[0]} width="100%" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={images[1]} width="100%" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={images[2]} width="100%" />
+      </Carousel.Item>
+    </Carousel>
+  );
+  return (
+    <Box height="auto" bg="lightGray" py={6}>
+      {renderCarousel()}
+    </Box>
+  );
+};
+export const CustomCarouselWithText = () => {
+  const onStepChanged = step => console.log(step);
+  const renderCarousel = () => (
+    <Carousel width={[1]} delay={3} onChange={onStepChanged}>
+      <Carousel.Item>
+        <Box py={3} textAlign="center">
+          <Text fontWeight="bolder">
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
+            print
+          </Text>
+        </Box>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Box py={3} textAlign="center">
+          <Text fontWeight="bolder">
+            2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
+            Virginia,
+          </Text>
+        </Box>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Box py={3} textAlign="center">
+          <Text fontWeight="bolder">or lipsum as it dummy text used in laying out print</Text>
+        </Box>
+      </Carousel.Item>
+    </Carousel>
+  );
+  return (
+    <Box height="auto" bg="lightGray" py={6}>
+      {renderCarousel()}
+    </Box>
+  );
+};
+
+export const CustomCarousel = () => {
+  const onStepChanged = step => console.log(step);
+  const renderCarousel = () => (
+    <Carousel width={[1]} onChange={onStepChanged}>
+      <Carousel.Item position="relative">
+        <img src={images[0]} width="100%" />
+        <Box position="absolute" display="flex" justifyContent="center" top={50} width={1}>
+          <Heading color="white">Title</Heading>
+        </Box>
+        <Box
+          position="absolute"
+          display="flex"
+          justifyContent="center"
+          textAlign="center"
+          bottom={50}
+          width={1}
+        >
+          <Text color="white" width={1 / 2}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
+            print
+          </Text>
+        </Box>
+      </Carousel.Item>
+      <Carousel.Item position="relative">
+        <img src={images[1]} width="100%" />
+        <Box position="absolute" display="flex" justifyContent="center" top={50} width={1}>
+          <Heading color="white">Title</Heading>
+        </Box>
+        <Box
+          position="absolute"
+          display="flex"
+          justifyContent="center"
+          textAlign="center"
+          bottom={50}
+          width={1}
+        >
+          <Text color="white" width={1 / 2}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
+            print
+          </Text>
+        </Box>
+      </Carousel.Item>
+      <Carousel.Item position="relative">
+        <img src={images[2]} width="100%" />
+        <Box position="absolute" display="flex" justifyContent="center" top={50} width={1}>
+          <Heading color="white">Title</Heading>
+        </Box>
+        <Box
+          position="absolute"
+          display="flex"
+          justifyContent="center"
+          textAlign="center"
+          bottom={50}
+          width={1}
+        >
+          <Text color="white" width={1 / 2}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
+            print
+          </Text>
+        </Box>
+      </Carousel.Item>
+    </Carousel>
+  );
+  return (
+    <Box height="auto" bg="lightGray" py={6}>
+      {renderCarousel()}
+    </Box>
+  );
+};
---
+++ src/components/carousel/index.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from 'react';
+import Box from '../box';
+import Space from '../space';
+import PropTypes from 'prop-types';
+import { IconArrow } from '../../icons';
+import { hooks } from '../../utils';
+
+const Item = props => <Box {...props} />;
+
+const BaseComponent = React.forwardRef(
+  (
+    { children, onChange, delay, leftIcon, rightIcon, arrowColor, navigationColor, ...props },
+    ref
+  ) => {
+    const [step, setStep] = useState(0);
+    const [prevStep, setPrevStep] = useState(0);
+    const { useInterval } = hooks;
+
+    const createCarouselItems = () => {
+      if (children.length === 0) {
+        return null;
+      }
+      return React.Children.map(children, (child, index) =>
+        React.cloneElement(child, {
+          display: `${index === step ? 'block' : 'none'}`,
+          animation: `${prevStep > step ? 'fadeInLeft' : 'fadeInRight'}`,
+        })
+      );
+    };
+
+    const createNavitationDots = () => {
+      return (
+        <Box display="flex" justifyContent="center">
+          <Space sr={1}>
+            {children.map((item, index) => (
+              <Box
+                key={index}
+                width="10px"
+                height="10px"
+                borderRadius="full"
+                bg={navigationColor ? navigationColor : 'black'}
+                css={`
+                  cursor: pointer;
+                  opacity: ${step === index ? 0.8 : 0.4};
+                  &:hover {
+                    opacity: 0.9;
+                  }
+                  &:active {
+                    opacity: 1;
+                  }
+                `}
+                onClick={() => setStep(index)}
+              />
+            ))}
+          </Space>
+        </Box>
+      );
+    };
+    const Next = () => {
+      setPrevStep(step);
+      if (step < children.length - 1) {
+        setStep(step + 1);
+      } else {
+        setStep(0);
+      }
+    };
+
+    const Prev = () => {
+      setPrevStep(step);
+      if (step > 0) {
+        setStep(step - 1);
+      } else {
+        setStep(children.length - 1);
+      }
+    };
+
+    useEffect(() => {
+      onChange(step);
+    }, [step]);
+
+    if (delay) {
+      useInterval(() => {
+        Next();
+      }, delay * 1000);
+    }
+
+    return (
+      <Box
+        display="display"
+        flexDirection="column"
+        justifyContent="space-between"
+        ref={ref}
+        {...props}
+      >
+        <Box display="flex" alignItems="center">
+          <Box p={0} onClick={() => Prev()}>
+            {leftIcon ? (
+              leftIcon
+            ) : (
+              <IconArrow
+                height={48}
+                width={48}
+                fill={arrowColor ? arrowColor : null}
+                css={`
+                  transform: rotate(-180deg);
+                  cursor: pointer;
+                `}
+              />
+            )}
+          </Box>
+          <Box width={1} display="flex" flexDirection="column" justifyContent="center" px={2}>
+            {createCarouselItems()}
+            {createNavitationDots()}
+          </Box>
+          <Box
+            p={0}
+            onClick={() => Next()}
+            css={`
+              cursor: pointer;
+            `}
+          >
+            {rightIcon ? rightIcon : <IconArrow height={48} width={48} />}
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
+);
+
+BaseComponent.displayName = 'Carousel';
+Item.displayName = 'Carousel.Item';
+
+BaseComponent.propTypes = {
+  onStepChange: PropTypes.func,
+  delay: PropTypes.number,
+  arrowColor: PropTypes.string,
+  navigationColor: PropTypes.string,
+};
+BaseComponent.defaultProps = {
+  delay: 0,
+  arrowColor: 'black',
+  navigationColor: 'black',
+};
+const Carousel = {
+  ...BaseComponent,
+  Item: null,
+};
+
+Carousel.Item = Item;
+
+export default Carousel;
