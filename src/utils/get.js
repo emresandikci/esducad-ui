9e6bb1ea5b3445ee25f5c921c63273d8d1031f72
emresandikci <me@emresandikci.com>
2020-04-22 02:18:12 +0300

    getCurrent Theme util added

--- src/utils/get.js
+++ src/utils/get.js
@@ -1,14 +1,19 @@
-import { default as theme } from './theme';
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 
 export const byThemeObj = (obj, matchWith) => {
   return Object.keys(obj).map(x => (x === matchWith ? x : null))[0];
 };
 
-export const color = (color, customTheme) => {
-  const { colors } = customTheme ? customTheme : theme;
+export const color = color => {
+  const { colors } = currentTheme();
   const themeColor = colors[`${color}`];
   if (themeColor !== undefined) {
     return themeColor;
   }
   return color;
+};
+
+export const currentTheme = () => {
+  return useContext(ThemeContext);
 };
