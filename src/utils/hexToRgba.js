43ee4c9773be5bd1b58124d469872ac9f3d15702
emresandikci <me@emresandikci.com>
2020-04-21 03:32:46 +0300

    hexToRgba updated

--- src/utils/hexToRgba.js
+++ src/utils/hexToRgba.js
@@ -1,11 +1,30 @@
 const hexToRgba = (hex, opacity) => {
-  const rgb = hex
-    .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1)
-    .match(/.{2}/g)
-    .map(x => parseInt(x, 16));
+  let r, g, b, color, rgb;
+  hex = hex.toLowerCase();
+  if (/^#([a-f0-9]{3}){1,2}$/.test(hex)) {
+    if (hex.length == 4) {
+      hex = '#' + [hex[1], hex[1], hex[2], hex[2], hex[3], hex[3]].join('');
+    }
+    hex = '0x' + hex.substring(1);
+    r = (hex >> 16) & 255;
+    g = (hex >> 8) & 255;
+    b = hex & 255;
 
-  return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]},${opacity})`;
+    if (opacity) {
+      rgb = `rgba(${r},${g},${b},${opacity})`;
+    } else {
+      rgb = `rgb(${r},${g},${b})`;
+    }
+    color = {
+      r,
+      g,
+      b,
+      alpha: opacity || 1,
+      all: rgb,
+    };
+    return color;
+  }
+  return console.error('wrong hex format', 'should be like #000 #000000');
 };
 
 export default hexToRgba;
