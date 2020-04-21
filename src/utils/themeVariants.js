9472765356b021818e6b43133c40ef61b54bce14
emresandikci <me@emresandikci.com>
2020-04-21 03:39:50 +0300

    button variants updated

--- src/utils/themeVariants.js
+++ src/utils/themeVariants.js
@@ -4,73 +4,44 @@ import animations from './animations';
 const buttonTypes = {
   default: {
     bg: 'lightGray',
-    '&:active': {
-      bg: 'gray',
-      opacity: 1,
-    },
   },
   primary: {
     variant: 'buttons.default',
     color: 'white',
     bg: 'primary',
-    '&:active': {
-      bg: 'darkPink',
-      opacity: 1,
-    },
   },
   secondary: {
     variant: 'buttons.default',
     color: 'white',
     bg: 'secondary',
-    '&:active': {
-      bg: 'darkBlue',
-      opacity: 1,
-    },
   },
   danger: {
     variant: 'buttons.default',
     bg: 'danger',
     color: 'white',
-    '&:active': {
-      bg: 'dangerDark',
-      opacity: 1,
-    },
   },
   success: {
     variant: 'buttons.default',
     bg: 'success',
     color: 'white',
-    '&:active': {
-      bg: 'successDark',
-      opacity: 1,
-    },
+  },
+  warning: {
+    variant: 'buttons.default',
+    bg: 'warning',
+    color: 'white',
   },
   info: {
     variant: 'buttons.default',
     bg: 'info',
     color: 'white',
-    '&:active': {
-      bg: 'infoDark',
-      opacity: 1,
-    },
   },
   outline: {
-    // variant: 'buttons.default',
     bg: 'white',
     boxShadow: 'inset 0 0 2px',
-    // '&:active': {
-    //   color: 'darkGray',
-    //   opacity: 1,
-    //   borderColor: 'darkGray',
-    // },
   },
   text: {
     padding: 0,
     bg: 'transparent',
-    // '&:active': {
-    //   color: 'darkGray',
-    //   opacity: 1,
-    // },
   },
 };
 
