5f30e057b682b99c17f640d82ba34f01ffc02aca
emresandikci <me@emresandikci.com>
2020-04-24 15:43:03 +0300

    fix: lint errors removed

--- src/components/space/index.js
+++ src/components/space/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Box from '../box';
-const Space = ({ sr, sl, st, sb, space, children, ...props }) => {
+const Space = ({ sr, sl, st, sb, space, children }) => {
   const setSpace = () => {
     if (children && children.length === 0) {
       return null;
     }
-    return React.Children.map(children, (child, index) => (
+    return React.Children.map(children, child => (
       <Box pr={sr} pl={sl} pt={st} pb={sb} p={space}>
         {React.cloneElement(child, {})}
       </Box>
