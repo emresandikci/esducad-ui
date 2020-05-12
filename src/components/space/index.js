11f58b04ec67fdf1d41620cd25d783766120fa6c
emresandikci <me@emresandikci.com>
2020-05-12 11:56:20 +0300

    refactor: new props added to space component

--- src/components/space/index.js
+++ src/components/space/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import Box from '../box';
-const Space = ({ sr, sl, st, sb, space, children }) => {
+import PropTypes from 'prop-types';
+
+const Space = ({ sr, sl, st, sb, sy, sx, space, children }) => {
   const setSpace = () => {
     if (children && children.length === 0) {
       return null;
     }
     return React.Children.map(children, (child) => (
-      <Box pr={sr} pl={sl} pt={st} pb={sb} p={space}>
+      <Box pr={sr} pl={sl} pt={st} pb={sb} py={sy} px={sx} p={space}>
         {React.cloneElement(child, {})}
       </Box>
     ));
   };
 
   return <React.Fragment>{setSpace()}</React.Fragment>;
+};
+
+Space.defaultProps = {
+  sr: 0,
+  sl: 0,
+  st: 0,
+  sb: 0,
+  sy: 0,
+  sx: 0,
+  spacing: null,
+};
+Space.prototype = {
+  sr: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  sl: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  st: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  sb: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  sy: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  sx: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  spacing: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default Space;
